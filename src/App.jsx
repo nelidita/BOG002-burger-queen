4cff4dbe38a11986da290c256c67ccbed7c73a6e
nelidita <nelidavalero06@gmail.com>
2021-07-31 14:23:00 -0500

    configuracion firebase y creacion de app en react

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css';
+import Inicio from './components/inicio'
+
+function App() {
+  return (
+    <div className="App">
+      <Inicio></Inicio>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/inicio.jsx
@@ -0,0 +1,10 @@
+import React, {Fragment} from 'react'
+
+function Inicio () {
+  return (
+    <Fragment className= "container">
+      <img src="" alt="" />
+    </Fragment>
+  )
+}
+export default Inicio
