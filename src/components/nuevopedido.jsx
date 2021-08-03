691237780c95904daee06fabf2c07ea56f5dfd55
Nelida Valero <nelidavalero06@gmail.com>
2021-08-02 19:45:56 -0500

    mejorando el menu json

--- src/components/nuevopedido.jsx
+++ src/components/nuevopedido.jsx
@@ -4,43 +4,47 @@ import "./nuevopedido.css"
 
 function NuevoPedido() {
 
-  const [menu, setMenu] = useState([])
+    const [menu, setMenu] = useState([])
 
-  // const obtenerMenu = async() => {
-  //   const data = await fetch(".../public/menu.json")
-  //   const dataMenu = await data.json()
-  //   console.log(dataMenu)
-  //   // setMenu(menu)
-  // }
-  // obtenerMenu()
+    // const obtenerMenu = async() => {
+    //   const data = await fetch(".../public/menu.json")
+    //   const dataMenu = await data.json()
+    //   console.log(dataMenu)
+    //   // setMenu(menu)
+    // }
+    // obtenerMenu()
 
-  useEffect(() => {
-    fetch('./menu.json')
-      .then(response => response.json())
-      .then(data => console.log(data[0]))
-  }, [])
+    useEffect(() => {
+        const dataMenu = fetch('./menu.json')
+            .then(response => response.json())
+            .then(data => data.menu)
+        setMenu(dataMenu)
+    }, [])
 
 
-  return (
-    <Fragment>
-      <div className="container">
-        <img src={logo} />
-        <div className="containerBotones">
-          <label htmlFor="name">Nombre del cliente:</label>
-          <input type="text" name="name" />
-          <label htmlFor="name">N° de Mesa:</label>
-          <input type="text" name="mesa" />
-        </div>
-        <div className="btnMenu">
-          <button className="btnMenu btn-dark btn-lg ">Desayunos</button>
-          <button className="btnMenu btn-dark btn-lg ">Hamburguesas</button>
-        </div>
-        <div className="desayunos">
-          <label htmlFor="desayunos"></label>
-          <input type="text" name="desayunos" />
-        </div>
-      </div>
-    </Fragment>
-  )
+    return (
+        <Fragment>
+            <div className="container">
+                <img src={logo} />
+                <div className="containerBotones">
+                    <label htmlFor="name">Nombre del cliente:</label>
+                    <input type="text" name="name" />
+                    <label htmlFor="name">N° de Mesa:</label>
+                    <input type="text" name="mesa" />
+                </div>
+                <div className="btnMenu">
+                    <button className="btnMenu btn-dark btn-lg ">Desayunos</button>
+                    <button className="btnMenu btn-dark btn-lg ">Hamburguesas</button>
+                </div>
+                <div className="desayunos">
+                        
+                    menu.map(item => {
+                        <button key={item.} ></button>
+                    })
+
+                </div>
+            </div>
+        </Fragment>
+    )
 }
 export default NuevoPedido
