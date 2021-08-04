814810f1ac2a117a1e71e4caf1708af074c6d19b
nelidita <nelidavalero06@gmail.com>
2021-08-03 19:21:09 -0500

    mostrando data

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="App">
       {/* <Inicio></Inicio> */}
-      <NuevoPedido></NuevoPedido>
+      <NuevoPedido/>
     </div>
   );
 }
--- src/components/nuevopedido.jsx
+++ src/components/nuevopedido.jsx
@@ -4,47 +4,41 @@ import "./nuevopedido.css"
 
 function NuevoPedido() {
 
-    const [menu, setMenu] = useState([])
+  const [menu, setMenu] = useState([])
 
-    // const obtenerMenu = async() => {
-    //   const data = await fetch(".../public/menu.json")
-    //   const dataMenu = await data.json()
-    //   console.log(dataMenu)
-    //   // setMenu(menu)
-    // }
-    // obtenerMenu()
+  useEffect(() => {
 
-    useEffect(() => {
-        const dataMenu = fetch('./menu.json')
-            .then(response => response.json())
-            .then(data => data.menu)
-        setMenu(dataMenu)
-    }, [])
+    fetch('./menu.json')
+      .then(response => response.json())
+      .then((data) =>setMenu(data[0]))
 
+  }, [])
 
-    return (
-        <Fragment>
-            <div className="container">
-                <img src={logo} />
-                <div className="containerBotones">
-                    <label htmlFor="name">Nombre del cliente:</label>
-                    <input type="text" name="name" />
-                    <label htmlFor="name">N° de Mesa:</label>
-                    <input type="text" name="mesa" />
-                </div>
-                <div className="btnMenu">
-                    <button className="btnMenu btn-dark btn-lg ">Desayunos</button>
-                    <button className="btnMenu btn-dark btn-lg ">Hamburguesas</button>
-                </div>
-                <div className="desayunos">
-                        
-                    menu.map(item => {
-                        <button key={item.} ></button>
-                    })
-
-                </div>
-            </div>
-        </Fragment>
-    )
+  return (
+    <Fragment>
+      <div className="container">
+        <img src={logo} />
+        <div className="containerBotones">
+          <label htmlFor="name">Nombre del cliente:</label>
+          <input type="text" name="name" />
+          <label htmlFor="name">N° de Mesa:</label>
+          <input type="text" name="mesa" />
+        </div>
+        <div className="btnMenu">
+          <button className="btnMenu btn-dark btn-lg ">Desayunos</button>
+          <button className="btnMenu btn-dark btn-lg ">Hamburguesas</button>
+        </div>
+        <div className="desayunos">
+            {
+              Object.keys(menu).map(item => (
+                <button key={item}>
+                  {item[name]}
+                </button>
+              ))
+            }
+        </div>
+      </div>
+    </Fragment>
+  )
 }
 export default NuevoPedido
